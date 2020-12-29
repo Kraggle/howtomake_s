b553285ddd83231a9923c22e34914a6c1148e313
Hayden Kibble <kibblehayden@gmail.com>
2020-12-29 16:29:03 +0000

    Google Analytics

--- views/partials/head.php
+++ views/partials/head.php
@@ -8,6 +8,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<? wp_head() ?>
 	<? htm_add_head_stuff() ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113501654-4"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-113501654-4');
+	</script>
 </head>
 
 <?
