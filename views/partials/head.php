53aa26cb7ba9b17199ed9558f2b5c57d5795d024
Hayden Kibble <kibblehayden@gmail.com>
2021-02-06 14:36:01 +0000

    resolved conflicts

--- views/partials/head.php
+++ views/partials/head.php
@@ -6,19 +6,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<<<<<<< HEAD
-	<? wp_head() ?>
-	<? htm_add_head_stuff() ?>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113501654-4"></script>
-	<script>
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
 
-	gtag('config', 'UA-113501654-4');
-	</script>
-=======
 	<?php wp_head() ?>
 	<?php htm_add_head_stuff() ?>
 
@@ -29,7 +17,7 @@
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	})(window,document,'script','dataLayer','GTM-T674ZX6');</script>
 	<!-- End Google Tag Manager -->
->>>>>>> ba60c29fd25126b8b34ad12da42a1bbb880040d7
+
 </head>
 
 <?php
